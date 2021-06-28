034becd541dd2df54f418b033336e556120e4dda
itsgabeover <gabe.enrique.miranda@gmail.com>
2021-06-28 12:47:58 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31,
+      num2 = 2,
+      num3 = 9,
+      num4 = 5;
+
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10);
+let mod = num3%num4;
+let max = Math.max(1, 20, 16);
